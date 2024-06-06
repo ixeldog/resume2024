4107a65ae54219ebacdae2021ae42a971c07b7eb
jgodleskiFieldEdge <joe.godleski@xplortechnologies.com>
2024-06-05 23:15:55 -0400

    Beginning of personalization: Profile, Experience, Skills, Education. Skill progress bar color based on percentage. CSS fixes for short and tall sections.

--- src/app/skill-progress-bar/skill-progress-bar.component.ts
+++ src/app/skill-progress-bar/skill-progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Skill } from '../models/skill';
 
@@ -14,11 +14,29 @@ export class SkillProgressBarComponent {
   @Input()
   Skill:Skill;
 
-  @Input()
+  static ColorClassPrefix:string = "progress-bar-";
   ColorClass:string;
 
   constructor() {
     this.Skill = new Skill();
-    this.ColorClass = "";
+    this.ColorClass = SkillProgressBarComponent.ColorClassPrefix;
+  }
+
+  ngOnChanges(changes:SimpleChanges) {
+    //either 1, 2, or 3 (for red, green blue)
+    let colorThird:number = Math.floor(this.Skill.Percent / 33);
+
+    //red
+    if (colorThird == 0) {
+      this.ColorClass += "3";
+    }
+    //green
+    else if (colorThird == 1) {
+      this.ColorClass += "1";
+    }
+    //blue - 2 for 66-98%, 3 for 99 and 100%
+    else if (colorThird >= 2) {
+      this.ColorClass += "2";
+    }
   }
 }
--- src/app/skills.service.ts
+++ src/app/skills.service.ts
@@ -6,29 +6,90 @@ import { SkillGroup } from './models/skill-group';
 })
 export class SkillsService {
 
-  SkillGroups:SkillGroup[] = [
+  SkillGroupsTest:SkillGroup[] = [
     {
       GroupLabel: "Professional Skills",
       Skills: [
-        {'Percent': 70, 'Label':'Communication'},
         {'Percent': 90, 'Label':'Leadership'},
         {'Percent': 85, 'Label':'Confidence'},
+        {'Percent': 70, 'Label':'Communication'},
       ]
     },
     {
       GroupLabel: "Software Skills",
       Skills: [
-        {'Percent': 85, 'Label':'Adobe Photoshop'},
         {'Percent': 90, 'Label':'Adobe Illustrator'},
+        {'Percent': 85, 'Label':'Adobe Photoshop'},
         {'Percent': 40, 'Label':'Adobe Fireworks'},
       ]
     },
     {
       GroupLabel: "Code Skills",
       Skills: [
+        {'Percent': 100, 'Label':'HTML5 / CSS3'},
+        {'Percent': 90, 'Label':'HTML5 / CSS3'},
         {'Percent': 90, 'Label':'HTML5 / CSS3'},
         {'Percent': 60, 'Label':'Javascript'},
+        {'Percent': 60, 'Label':'Javascript'},
         {'Percent': 40, 'Label':'PHP'},
+        {'Percent': 30, 'Label':'PHP'},
+        {'Percent': 10, 'Label':'PHP'},
+      ]
+    }
+  ];
+
+
+  SkillGroups:SkillGroup[] = [
+    {
+      GroupLabel: "Backend languages",
+      Skills: [
+        {'Percent': 75, 'Label':'C#'},
+        {'Percent': 50, 'Label':'SQL'},
+        {'Percent': 25, 'Label':'Java'},
+        {'Percent': 25, 'Label':'XSLT'},
+      ]
+    },
+    {
+      GroupLabel: "Frontend languages",
+      Skills: [
+        {'Percent': 75, 'Label':'JavaScript/TypeScript'},
+        {'Percent': 50, 'Label':'CSS/SASS'},
+        {'Percent': 25, 'Label':'HTML'},
+      ]
+    },
+    {
+      GroupLabel: "Frontend Frameworks",
+      Skills: [
+        {'Percent': 75, 'Label':'Vue 2'},
+        {'Percent': 50, 'Label':'React 15'},
+        {'Percent': 50, 'Label':'Knockout 3'},
+        {'Percent': 25, 'Label':'Angular 17'},
+      ]
+    },
+    {
+      GroupLabel: "Frontend tools",
+      Skills: [
+        {'Percent': 50, 'Label':'Webpack'},
+        {'Percent': 50, 'Label':'NPM'},
+        {'Percent': 50, 'Label':'ngrok'},
+      ]
+    },
+    {
+      GroupLabel: "Azure",
+      Skills: [
+        {'Percent': 75, 'Label':'Functions'},
+        {'Percent': 75, 'Label':'Virtual Machines'},
+        {'Percent': 50, 'Label':'Service Bus'},
+        {'Percent': 50, 'Label':'Event Grid'},
+      ]
+    },
+    {
+      GroupLabel: "DB",
+      Skills: [
+        {'Percent': 75, 'Label':'SQL Server'},
+        {'Percent': 50, 'Label':'Cosmos'},
+        {'Percent': 50, 'Label':'MySQL'},
+        {'Percent': 25, 'Label':'Oracle'},
       ]
     }
   ];
