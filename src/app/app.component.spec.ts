75fea42d6ce788e06e296bc1b485fff89c3bb34f
jgodleskiFieldEdge <joe.godleski@xplortechnologies.com>
2024-11-12 13:12:24 -0500

    Fix broken unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, resume2024');
+    expect(compiled.querySelector('h1')?.textContent).toContain("Hi, i'm Joe Godleski");
   });
 });
